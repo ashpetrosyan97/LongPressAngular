42a170d360f1991f388624684199691a27863c5d
Ashot Petrosyan <ashotnew97@mail.ru>
2019-06-15 15:10:10 +0400

    fix

--- src/app/long-press.directive.ts
+++ src/app/long-press.directive.ts
@@ -1,31 +1,33 @@
 import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
 
 @Directive({
-  selector: '[LongPress]'
+    selector: '[LongPress]'
 })
 export class LongPressDirective {
 
 
-  private start: number;
-  private end: number;
-  @Output() onLongPress = new EventEmitter<any>();
-  @Output() onClick = new EventEmitter<any>();
+    private start: number;
+    private end: number;
+    @Output() onLongPress = new EventEmitter<any>();
+    @Output() onClick = new EventEmitter<any>();
 
-  @HostListener('mousedown') onMouseDown() {
-    this.start = Date.now();
-  }
-
-  @HostListener('mouseup') onMouseUp() {
-    this.end = Date.now();
-    if (this.end - this.start > 500) {
-      this.onLongPress.emit();
-    } else {
-      this.onClick.emit();
+    @HostListener('touchstart') onTouchstart() {
+        event.preventDefault();
+        this.start = Date.now();
     }
-  }
 
-  constructor() {
-  }
+    @HostListener('touchend') onTouchend() {
+        event.preventDefault();
+        this.end = Date.now();
+        if (this.end - this.start > 500) {
+            this.onLongPress.emit();
+        } else {
+            this.onClick.emit();
+        }
+    }
+
+    constructor() {
+    }
 
 
 }
